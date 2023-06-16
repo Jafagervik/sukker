c343db3988f25936671dd5f8020949be52312300
Jafagervik <jorgenafagervik@gmail.com>
2023-06-17 08:14:20 +0900

    Standardize benchmarks

--- benches/blocked_matmul_benches.rs
+++ benches/blocked_matmul_benches.rs
@@ -4,8 +4,8 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 // Benchmark for matrix multiplication
 fn blocked_matmul_bench(c: &mut Criterion) {
-    let x = black_box(Matrix::<f32>::randomize((100, 100)));
-    let y = black_box(Matrix::<f32>::randomize((100, 100)));
+    let x = black_box(Matrix::<f64>::randomize((1000, 1000)));
+    let y = black_box(Matrix::<f64>::randomize((1000, 1000)));
 
     c.bench_function("blocked matmul transpose", |b| {
         b.iter(|| x.matmul(&y).unwrap())
--- benches/nn_sparse_matmul.rs
+++ benches/nn_sparse_matmul.rs
@@ -1,49 +1,49 @@
 use std::collections::HashMap;
-use sukker::{SparseMatrix, SparseMatrixData};
+use sukker::{smd, SparseMatrix, SparseMatrixData};
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 // Benchmark for matrix multiplication
 fn nn_sparse_matmul_bench(c: &mut Criterion) {
-    let mut indexes: SparseMatrixData<f64> = HashMap::new();
-
-    indexes.insert((0, 1), 2.0);
-    indexes.insert((1, 9), 4.0);
-    indexes.insert((1, 8), 6.0);
-    indexes.insert((9, 9), 8.0);
-    indexes.insert((6, 2), 8.0);
-    indexes.insert((2, 2), 8.0);
-    indexes.insert((1, 3), 8.0);
-    indexes.insert((8, 6), 8.0);
-    indexes.insert((9, 1), 8.0);
-    indexes.insert((2, 3), 8.0);
-    indexes.insert((7, 9), 8.0);
-    indexes.insert((7, 8), 8.0);
-    indexes.insert((3, 8), 8.0);
-    indexes.insert((0, 9), 8.0);
+    let indexes: SparseMatrixData<f64> = smd![
+        ((0, 1), 2.0),
+        ((1, 9), 4.0),
+        ((1, 8), 6.0),
+        ((9, 9), 8.0),
+        ((6, 2), 8.0),
+        ((2, 2), 8.0),
+        ((1, 3), 8.0),
+        ((8, 6), 8.0),
+        ((9, 1), 8.0),
+        ((2, 3), 8.0),
+        ((7, 9), 8.0),
+        ((7, 8), 8.0),
+        ((3, 8), 8.0),
+        ((0, 9), 8.0)
+    ];
 
     let x = black_box(SparseMatrix::<f64>::init(indexes, (1000, 1000)));
 
-    let mut indexes2: SparseMatrixData<f64> = HashMap::new();
-
-    indexes2.insert((0, 0), 2.0);
-    indexes2.insert((1, 0), 4.0);
-    indexes2.insert((1, 1), 8.0);
-    indexes2.insert((2, 1), 6.0);
-    indexes2.insert((8, 1), 3.0);
-    indexes2.insert((2, 5), 1.0);
-    indexes2.insert((8, 1), 12.0);
-    indexes2.insert((9, 8), 4.0);
-    indexes2.insert((8, 2), 2.0);
-    indexes2.insert((2, 4), 6.0);
-    indexes2.insert((3, 9), 1.0);
-    indexes2.insert((7, 0), 6.0);
-    indexes2.insert((7, 0), 8.0);
-    indexes2.insert((9, 7), 6.0);
+    let indexes2: SparseMatrixData<f64> = smd![
+        ((0, 0), 2.0),
+        ((1, 0), 4.0),
+        ((1, 1), 8.0),
+        ((2, 1), 6.0),
+        ((8, 1), 3.0),
+        ((2, 5), 1.0),
+        ((8, 1), 12.0),
+        ((9, 8), 4.0),
+        ((8, 2), 2.0),
+        ((2, 4), 6.0),
+        ((3, 9), 1.0),
+        ((7, 0), 6.0),
+        ((7, 0), 8.0),
+        ((9, 7), 6.0)
+    ];
 
     let y = black_box(SparseMatrix::<f64>::init(indexes2, (1000, 1000)));
 
-    c.bench_function(" NxN @ NxN sparse matmul", |b| {
+    c.bench_function("NxN @ NxN sparse matmul", |b| {
         b.iter(|| x.matmul_sparse(&y).unwrap())
     });
 }
